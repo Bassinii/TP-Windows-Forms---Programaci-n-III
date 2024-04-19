15b50da2c45f88030681b9ea51a4903dd73755e4
unknown <brunobassini86@gmail.com>
2024-04-19 00:12:01 -0300

    Arranque de trabajo

---
+++ Articulos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPWindowsFormsProgramacionIII
+{
+    internal class Articulos
+    {
+        public string codigo { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public Marcas marca { get; set; }
+        public Categorias categoria { get; set; }
+        List <string> listImagenes = new List<string>();
+
+        public Articulos() {;}
+
+    }
+}
