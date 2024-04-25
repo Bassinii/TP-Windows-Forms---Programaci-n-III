4e3038b4fbac0c3d3ce41bd62b5015754f053680
sergiosebs2 <159736850+sergiosebs2@users.noreply.github.com>
2024-04-24 21:08:21 -0300

    Problemas para mostrar la imagen de articulo
    Agregado de la atributo ver imagen al seleccionar un item de la lista. Hay una exception que no estoy pudiendo solucionar, y al intentar cargar la imagen siempre va al catch() y ejecuta el metodo mostrar la imagen de placeholder. Desde la DB el comando de ejecucion es correcto y muestra la tabla con los datos esperados. Hay un error en la carga del string de ImagenUrl.
    Agregado de PictureBox correspondiente. 
    Eliminados archivos viejos que impedian la compilacion del commit anterior.

---
+++ TP2 PIII/Negocio/NegocioArticulo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using ClasesdeDominio;
+
+namespace Negocio
+{
+    public class NegocioArticulo
+    {
+        //private string ruta = "Data Source=localhost\\sqlexpress; Initial Catalog = CATALOGO_P3_DB; Integrated Security = True";
+        //   public NegocioArticulo()
+        // public string GetRuta() { return ruta; }
+        public List<Articulo> listar()
+        {   //DECLARO LOS OBJETOS NECESARIOS PARA LA CONEXION
+            List<Articulo> lista = new List<Articulo>();
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;
+
+            try //LOS CONFIGURO IdMarca, IdCategoria,
+            {
+           
+                conexion.ConnectionString = "server=.\\SQLEXPRESS; database=CATALOGO_P3_DB; integrated security=true ";
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = "SELECT A.ID, A.Codigo, A.Nombre, A.Descripcion, A.Precio, I.ImagenUrl FROM ARTICULOS A LEFT JOIN IMAGENES I  ON A.ID = I.ID ";
+
+                
+                comando.Connection = conexion;
+
+                conexion.Open();
+                lector = comando.ExecuteReader();
+
+                while (lector.Read())
+                {
+                    Articulo aux = new Articulo();
+                    aux.id = lector.GetInt32(0);
+                    aux.codigo = (string)lector["Codigo"];
+                    aux.nombre = (string)lector["Nombre"];
+                    aux.descripcion = (string)lector["Descripcion"];
+                    aux.precio = (float)lector.GetDecimal(4);
+                    aux.imagenArticulo = new Imagenes { urlImagen = lector.GetString(5) };
+                    
+
+
+
+
+                    lista.Add(aux);
+                }
+                conexion.Close();
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
+           
+        
+            
---
+++ TP2 PIII/TPWindowsForms-Programacion-III/VentanaListarArticulos.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Negocio;
+using ClasesdeDominio;
+
+namespace TPWindowsFormsProgramacionIII
+{
+    public partial class VentanaListarArticulos : Form
+    {   
+        private List<Articulo> listaArticulos;
+        public VentanaListarArticulos()
+        {
+            InitializeComponent();
+            //CargarGrilla("select * from ARTICULOS");
+        }
+        /*public void CargarGrilla(string consulta)
+        {
+            NegocioArticulo miaccesoDatos= new NegocioArticulo();
+            SqlConnection Cn = new SqlConnection(miaccesoDatos.GetRuta());
+            SqlCommand cmd = new SqlCommand(consulta,Cn);
+            Cn.Open();
+            SqlDataReader dataReader= cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(dataReader);
+            gdvListadoDeArticulos.DataSource = dt;
+            Cn.Close();
+        }
+        */
+
+        private void ListarArticulos_Load(object sender, EventArgs e)
+        {
+            NegocioArticulo negocio = new NegocioArticulo();
+            listaArticulos =negocio.listar();
+            
+                gdvListadoDeArticulos.DataSource = listaArticulos;
+            
+            cargarImagen(listaArticulos[0].imagenArticulo.urlImagen);
+            
+        }
+        private void gdvListadoDeArticulos_SelectionChanged(object sender, EventArgs e) 
+        {
+            Articulo seleccionado = (Articulo)gdvListadoDeArticulos.CurrentRow.DataBoundItem;
+            cargarImagen(seleccionado.imagenArticulo.urlImagen);
+
+        }
+        private void cargarImagen(string imagen)
+        {
+            try
+            {
+                pictureBoxImagen.Load(imagen);
+            }
+            catch (Exception ex)
+            {
+                pictureBoxImagen.Load("https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg");
+
+            }
+            
+        }
+    }
+
+}
