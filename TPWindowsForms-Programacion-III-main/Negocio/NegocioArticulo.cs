15031f07b141cb91c970c8cfff0438fd1d94fafa
sergiosebs2 <159736850+sergiosebs2@users.noreply.github.com>
2024-04-26 21:58:52 -0300

    Muestra los articulos cargados en la lista.
    Ahora muestra correctamente los Articulos Agregados en la lista, crea al mismo tiempo un elemento Imagen en la DB, con su ImagenUrl cargada desde la aplicacion. 
    Reincie el contador id autonumerico en la DB, usando la instruccion: DBCC CHECKIDENT ('ARTICULOS', RESEED, 7); 
    Falta que muestre correctamente los campos Marca y Categoria. 
    Cambie provisoriamente el comando enviado al SQL para hacer la consulta.

---
+++ TPWindowsForms-Programacion-III-main/Negocio/NegocioArticulo.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using ClasesdeDominio;
+using System.Windows.Forms;
+
+namespace Negocio
+{
+    public class NegocioArticulo
+    {
+        //private string ruta = "Data Source=localhost\\sqlexpress; Initial Catalog = CATALOGO_P3_DB; Integrated Security = True";
+        //   public NegocioArticulo()
+        // public string GetRuta() { return ruta; }
+        public List<Articulo> listar()
+        {   
+
+            List<Articulo> lista = new List<Articulo>();
+            AccesoDatos datosArticulo = new AccesoDatos();
+            try
+            {
+                //datosArticulo.setearConsulta("SELECT A.Id,Codigo,Nombre,A.Descripcion, M.Descripcion Marca,C.Descripcion Categoria,Precio,I.ImagenUrl FROM Articulos A, Categorias C, Marcas M, IMAGENES I WHERE A.Id = C.Id AND M.Id = A.Id AND I.Id=A.Id");
+                datosArticulo.setearConsulta("SELECT A.Id,Codigo,Nombre,A.Descripcion,A.Descripcion, A.IdMarca , A.Precio,I.ImagenUrl FROM Articulos A, IMAGENES I WHERE A.Id = I.Id");
+
+                
+                datosArticulo.ejecutarLectura();
+
+                while (datosArticulo.Lector.Read())
+                {
+                    Articulo aux = new Articulo();
+                    aux.id = datosArticulo.Lector.GetInt32(0);
+                    aux.codigo = (string)datosArticulo.Lector["Codigo"];
+                    aux.nombre = (string)datosArticulo.Lector["Nombre"];
+                    aux.descripcion = (string)datosArticulo.Lector["Descripcion"];
+                    //aux.marca = new Marca { id = int.Parse(textbox) };
+                    //aux.categoria = new Categoria { descripcion = (string)datosArticulo.Lector["Categoria"] };
+                    aux.precio = (float)datosArticulo.Lector.GetDecimal(6);
+                    aux.imagenArticulo = new Imagenes { urlImagen = (string)datosArticulo.Lector["ImagenUrl"] };
+
+
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datosArticulo.CerrarConexion();
+            }
+        }
+
+
+        /* public void agregar(Articulo aux)
+         {
+             AccesoDatos datos = new AccesoDatos();
+             try
+             {
+
+                 datos.setearConsulta("INSERT INTO ARTICULOS (Codigo, Nombre, Descripcion, IdMarca, IdCategoria, Precio) VALUES (" + aux.codigo + ", '" + aux.nombre + "', '" + aux.descripcion + "', " + aux.marca.id + ", " + aux.categoria.id + ", " + aux.precio + ")");
+                 datos.ejecutarAccion();
+             }        
+             catch (Exception ex)
+             {
+
+                 throw ex;
+             }
+             finally
+             {
+                 datos.CerrarConexion();
+             }
+         }
+       
+        public void agregar(Articulo aux, string urlImagen)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                // Paso 1: Insertar el artículo en la tabla ARTICULOS
+                datos.setearConsulta("INSERT INTO ARTICULOS (Codigo, Nombre, Descripcion, IdMarca, IdCategoria, Precio) VALUES ('" + aux.codigo + "', '" + aux.nombre + "', '" + aux.descripcion + "', " + aux.marca.id + ", " + aux.categoria.id + ", " + aux.precio + ")");
+                datos.ejecutarAccion();
+                datos.CerrarConexion();
+
+                // Paso 2: Obtener el ID autonumérico del artículo recién insertado
+                datos.setearConsulta("SELECT SCOPE_IDENTITY()");
+                datos.ejecutarLectura();
+                int idArticulo = 0;
+                if (datos.Lector.Read())
+                {
+                    idArticulo = datos.Lector.IsDBNull(0) ? 0 : Convert.ToInt32(datos.Lector[0]);
+                }
+                datos.Lector.Close();
+                datos.CerrarConexion();
+
+                // Paso 3: Insertar la URL de la imagen junto con el ID del artículo en la tabla IMAGENES
+                datos.setearConsulta("INSERT INTO IMAGENES (Id, ImagenUrl) VALUES (" + idArticulo + ", '" + urlImagen + "')");
+                datos.ejecutarAccion();
+                datos.CerrarConexion();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                throw ex;
+            }
+            finally
+            {
+                datos.CerrarConexion();
+            }
+        }
+         */
+        public void agregar(Articulo aux, string urlImagen)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                // Paso 1: Insertar el artículo en la tabla ARTICULOS
+                datos.setearConsulta("INSERT INTO ARTICULOS (Codigo, Nombre, Descripcion, IdMarca, IdCategoria, Precio) VALUES ('" + aux.codigo + "', '" + aux.nombre + "', '" + aux.descripcion + "', " + aux.marca.id + ", " + aux.categoria.id + ", " + aux.precio + ")");
+                datos.ejecutarAccion();
+                datos.CerrarConexion();
+
+                /* Paso 2: Obtener el ID autonumérico del artículo recién insertado
+                datos.setearConsulta("SELECT SCOPE_IDENTITY()");
+                datos.ejecutarLectura();
+                int idArticulo = 0;
+                if (datos.Lector.Read())
+                {
+                    idArticulo = Convert.ToInt32(datos.Lector[0]);
+                }
+                datos.Lector.Close();
+                datos.CerrarConexion();
+                */
+
+                // Paso 3: Insertar la URL de la imagen junto con el ID del artículo en la tabla IMAGENES
+                datos.setearConsulta("INSERT INTO IMAGENES (ImagenUrl, IdArticulo) VALUES ('" + urlImagen + "', " + 3 + ")");
+                datos.ejecutarAccion();
+                datos.CerrarConexion();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                throw ex;
+            }
+            finally
+            {
+                datos.CerrarConexion();
+            }
+        }
+
+    }
+}      
---
+++ TPWindowsForms-Programacion-III-main/TPWindowsForms-Programacion-III/VentanaPrincipal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClasesdeDominio;
+using Negocio;
+
+namespace TPWindowsFormsProgramacionIII
+{
+    public partial class VentanaPrincipal : Form
+    {
+        public VentanaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void listaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            VentanaListarArticulos Lista = new VentanaListarArticulos();
+            Lista.Show();
+        }
+
+        private void agregarArticulosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
